13e8d890b89784870d57d282a9c306d9e8b03726
Stan Kornelsen <stankornelsen@Stans-MacBook-Pro.local>
2026-07-20 11:16:29 -0400

    Fix LP3 keyboard density scaling

--- app/src/main/kotlin/com/stan/libbylight/ui/LightEmbeddedLp3Keyboard.kt
+++ app/src/main/kotlin/com/stan/libbylight/ui/LightEmbeddedLp3Keyboard.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.thelightphone.lp3Keyboard.ui.DarkKeyboardColors
 import com.thelightphone.lp3Keyboard.ui.LightKeyboardColors
@@ -27,20 +30,27 @@ fun LightEmbeddedLp3Keyboard(viewModel: Lp3KeyboardViewModel) {
     } else {
         DarkKeyboardColors
     }
-    Lp3KeyboardTheme(keyboardColors) {
-        val colors = LocalKeyboardColors.current
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colors.background)
-                .padding(top = 10.dp),
-        ) {
-            Lp3KeyboardWrapper(
-                layout = layout,
-                keyboardOptions = keyboardOptions,
-                layoutOptions = layoutOptions,
-                callback = viewModel,
-            )
+    val systemDensity = LocalDensity.current
+    val keyboardDensity = Density(
+        density = maxOf(systemDensity.density, 3f),
+        fontScale = systemDensity.fontScale,
+    )
+    CompositionLocalProvider(LocalDensity provides keyboardDensity) {
+        Lp3KeyboardTheme(keyboardColors) {
+            val colors = LocalKeyboardColors.current
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(colors.background)
+                    .padding(top = 10.dp),
+            ) {
+                Lp3KeyboardWrapper(
+                    layout = layout,
+                    keyboardOptions = keyboardOptions,
+                    layoutOptions = layoutOptions,
+                    callback = viewModel,
+                )
+            }
         }
     }
 }
